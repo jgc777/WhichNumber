d669d0d98433a80e21bbcb9e6ea1d6eeff02e445
jgc777 <90793213+jgc777@users.noreply.github.com>
2025-01-11 14:01:29 +0100

    Website addition, screensaver launching, comments, some other fixes, changes and optimizations

--- Program.cs
+++ Program.cs
@@ -8,42 +8,79 @@ namespace WhichNumber {
                 Console.Title = "Which Number?";
                 Banner();
                 switch (args.Length) {
-                    case 0:
+                    case 0: // No arguments, open menu
                         Menu();
                         break;
-                    case 1:
-                        switch (args[0][0]) {
-                            case 'a':
+                    case 1: // One argument
+                        switch (args[0].ToLower()) {
+                            case "a":
+                                // Run guess the number with no number
                                 GuessTheNumber(-1);
                                 break;
-                            case 'b':
+                            case "b":
+                                // Run binary search with default starting point
                                 BinarySearch(100);
+                                break;
+                            case "c":
+                                Help();
+                                break;
+                            case "/s":
+                                // Runned as screensaver, open menu
+                                Menu();
+                                break;
+                            case "/?":
+                                Help();
+                                break;
+                            case "-?":
+                                Help();
+                                break;
+                            case "--help":
+                                Help();
+                                break;
+                            case "-h":
+                                Help();
+                                break;
+                            case "/h":
+                                Help();
+                                break;
+                            case "/help":
+                                Help();
                                 break;
                             default:
                                 Console.WriteLine("Invalid first argument!");
+                                Help();
                                 break;
                         }
                         break;
-                    case 2:
-                        switch (args[0][0]) {
+                    case 2: // Two arguments
+                        switch (args[0].ToString().ToLower()[0]) { // First argument, lower case, first character
                             case 'a':
+                                // Run guess the number with the number provided in the second argument
                                 GuessTheNumber(Int32.Parse(args[1]));
                                 break;
                             case 'b':
+                                // Run binary search starting with the number provided in the second argument
                                 BinarySearch(Int32.Parse(args[1]));
                                 break;
-                            default:
-                                Console.WriteLine("Invalid first argument!");
+                            default: // like for example "-? 100"
+                                Console.WriteLine("Invalid first argument for 2 arguments execution!");
+                                Help();
                                 break;
                         }
                         break;
                     default:
                         Console.WriteLine("Too many arguments!");
+                        Help();
                         break;
                 }
             } catch (Exception ex) {
                 Console.Write($"\nError: {ex.Message}\n");
+                // Automatically close
             }
+        }
+        static void Help() {
+            Console.WriteLine("Opening project website...");
+            System.Diagnostics.Process.Start("http://jgc.linkpc.net/WhichNumber/");
         }
         static void Banner() {
             Console.WriteLine(@"
@@ -60,13 +97,22 @@ namespace WhichNumber {
         static void Menu() {
             Console.WriteLine(@"Do you want to:
 a) Guess the computer number
-b) Make your computer guess your number");
-            switch (Console.ReadKey().KeyChar.ToString().ToLower()) {
-                case "a":
+b) Make your computer guess your number
+c) Get more info
+d) Exit");
+            switch (Console.ReadKey().KeyChar.ToString().ToLower()[0]) {
+                // Read key, then convert to string, then convert to lower case, then get first character
+                case 'a':
                     GuessTheNumber(-1);
                     break;
-                case "b":
+                case 'b':
                     BinarySearch(100);
+                    break;
+                case 'c':
+                    Help();
+                    break;
+                case 'd':
+                    Environment.Exit(0);
                     break;
                 default:
                     Console.WriteLine("Invalid option!");
@@ -86,11 +132,8 @@ b) Make your computer guess your number");
                 switch (Console.ReadKey().KeyChar) {
                     case '+':
                         min = reference + 1;
-                        if (max == -1) {
-                            reference = reference * 2;
-                        } else {
-                            reference = (max + min) / 2;
-                        }
+                        if (max == -1) reference = reference * 2; // Maximum not set, so double the reference
+                        else reference = (max + min) / 2;
                         break;
                     case '-':
                         max = reference - 1;
@@ -99,19 +142,20 @@ b) Make your computer guess your number");
                     case '=':
                         Console.Clear();
                         Console.WriteLine($"Found your number ({reference}) on {attempt} attempts!");
-                        finished = true;
+                        finished = true; // Exit loop
                         break;
                     default:
                         Console.WriteLine("Invalid option!");
-                        attempt--;
+                        attempt--; // Do not count this attempt
                         break;
                 }
             } while (!finished);
             return reference;
         }
-        static void GuessTheNumber(int number) {
+        static int GuessTheNumber(int number) {
             Console.Clear();
             if (Math.Abs(number) != number) {
+                // Negative number, so generate a random number
                 Console.Write("Minimum number:\n>");
                 int min = Int32.Parse(Console.ReadLine());
                 Console.Write("Maximum number:\n>");
@@ -130,28 +174,26 @@ b) Make your computer guess your number");
                 input = Console.ReadLine();
                 if ((input.Length == 0)) {
                     Console.WriteLine("Empty input.");
-                    attempt--;
-                    System.Threading.Thread.Sleep(2000);
-                    GuessTheNumber(number);
-
                 } else {
-                    attempt++;
+                    attempt++; // Count attempt
+                    Console.WriteLine($"Attempt #{attempt}, {stopwatch.Elapsed:hh\\:mm\\:ss} elapsed");
                     guess = Int32.Parse(input);
                     Console.Clear();
                     if (guess == number) {
                         stopwatch.Stop();
                         Console.WriteLine($"You found the number {number} in {attempt} attempts ({stopwatch.Elapsed:hh\\:mm\\:ss})!");
-                        break;
+                        break; // Exit loop
                     } else if (guess > number) {
                         Console.WriteLine($"Lower than {guess}!");
                     } else if (guess < number) {
                         Console.WriteLine($"Higher than {guess}!");
                     } else {
                         Console.WriteLine("Error. Try again.");
-                        attempt--;
+                        attempt--; // Do not count this attempt
                     }
                 }
             } while (true);
+            return attempt;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # WhichNumber
  Simple Binary Search and number guessing game algorithm made in C#.
+## Downloading
+ Check the [release](https://github.com/jgc777/WhichNumber/releases/latest)
 ## Syntax
- `WhichNumber.exe (no args)` or `WhichNumber.exe (a/b) (solution/start reference)`
+ `WhichNumber.exe (a/b/c/-?/-h/--help) (solution/start reference)`
+
